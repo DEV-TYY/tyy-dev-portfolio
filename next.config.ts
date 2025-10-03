cdc53456db50ac6e7112022a1a3dc342c03076f6
Suonty2 <suontii2023@gmail.com>
2025-10-03 23:04:27 +0700

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   output: 'export',
+  distDir: 'out',
+    basePath: '/dev-ty',     // repo name, no trailing slash
+    assetPrefix: '/dev-ty/', // ensures assets load from repo path
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
