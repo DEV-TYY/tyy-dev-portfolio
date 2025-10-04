4405bee9cda78d601a6f8e4695157c7e8a59e647
Suonty2 <suontii2023@gmail.com>
2025-10-04 15:18:23 +0700

    fix: remove unused wheelTextureURLs and deckTextureURLs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   distDir: 'out',
   basePath: '/tyy-dev-portfolio',     // repo name, no trailing slash
   assetPrefix: '/tyy-dev-portfolio/', // ensures assets load from repo path
